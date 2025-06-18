280cc88cbff78e3b6beec6cfc92a7de9f6cace00
makasinui <mks.shi@ya.ru>
2025-06-18 20:45:48 +0300

    feat: add icons

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,21 @@
+import IconClose from './close.svg';
+import IconLighting from './lighting.svg';
+import IconMinus from './minus.svg';
+import IconPause from './pause.svg';
+import IconPlay from './play.svg';
+import IconPlus from './plus.svg';
+import IconRandom from './random.svg';
+import IconSortAsc from './sort_asc.svg';
+import IconSortDesc from './sort_desc.svg';
+
+export {
+    IconClose,
+    IconLighting,
+    IconMinus,
+    IconPause,
+    IconPlay,
+    IconPlus,
+    IconRandom,
+    IconSortAsc,
+    IconSortDesc
+}
